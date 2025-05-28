0b5439567723b9bada7542508b788bf45b786853
Gabriel Nunes <gabrielnunes.service@gmail.com>
2025-05-27 21:39:50 -0300

    main.ts e app.routes.ts ajustados

--- biblioteca-frontend/src/app/app.routes.ts
+++ biblioteca-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LivroListComponent } from './components/livros/livro-list/livro-list.component';
+import { LivroFormComponent } from './components/livros/livro-form/livro-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'livros', pathMatch: 'full' },
+  { path: 'livros', component: LivroListComponent },
+  { path: 'livros/novo', component: LivroFormComponent },
+  { path: 'livros/editar/:id', component: LivroFormComponent },
+  { path: '**', redirectTo: 'livros' }
+];
